01e14821d2483ce386eabd1a1eb97b821a12ea56
AdamxzV3 <99908796+AdamxzV3@users.noreply.github.com>
2022-11-14 13:37:06 +0100

    Create Podmíněné Příkazy.lua

---
+++ Podmíněné Příkazy.lua
@@ -0,0 +1,29 @@
+--dneska budeme probirat klicove slova napr.. {If, True, False, Then, ==, or, and} atd atd..
+
+--
+
+if true then
+	
+	print("tenhle kod se vypise") ----pokud je to pravda tak se kod prehraje pokud tam je false tak se ten kod neprehraje
+	
+end
+------------------------------
+if false then
+	
+	print("Tenhle kod se nevypise")
+	
+end
+------------------------------
+local variable = 5 --" local "x" muzeme pojmenovat jak koliv asi, nejsem si jisty a  napiseme nejake cislo napr "5"
+
+if variable == 6 then --pokud vase "x" je "6" tak vypise se incorect
+	print("incorrect")
+elseif true then --ale pokud vase variable je 5, tak se vypise correct
+	local variable = 5
+	if variable == 5 then
+		print("correct")
+	end
+
+end
+----------------------------------------------------------------------------------------------------------------------
+--dokoncim zitra, mozna dnes nemam moc casu
